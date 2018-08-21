dc599a19bdde485e6843174f05e299150ef152d3
Adrian Krige <krigeadrian@gmail.com>
2018-08-22 00:37:56 +0200

    Add Shop menu scene
    Added the shop scene and started messing with PlayerPrefs and a static script

---
+++ Neo Divitias/Assets/Scripts/GameData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameData {
+    public static int Level { get; set; }
+    public static void IncrLevel(){
+        Level++;
+    }
+}
+
+public static class PlayerOne {
+    public static int Weapon { get; set; }
+    public static int Armour { get; set; }
+    public static int Movement { get; set; }
+    public static int Kills { get; set; }
+}
--- Neo Divitias/Assets/Scripts/MainMenu.cs
+++ Neo Divitias/Assets/Scripts/MainMenu.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    //PlayerPrefs.SetFloat("currentlevel", 1);
+    
+
 	public void PlayGame(){
-		SceneManager.LoadScene ("Level 1");
+        PlayerPrefs.SetInt("next_level", 1);
+		SceneManager.LoadScene ("Shop");
 	}
+
+    public void StartNextLevel()
+    {
+        // Get level from static script or persistent data and load the next level
+        int next_level = PlayerPrefs.GetInt("next_level");
+        SceneManager.LoadScene(string.Format("Level {0}", next_level));
+    }
 
     public void QuitGame(){
         Debug.Log("Quitting!");
