0be98c57f9921e744437653db1d2de16ad923d28
Matthew Morris <MRRMAT011@myuct.ac.za>
2018-10-12 23:23:22 +0200

    Some bug fixes. Controller input in game

--- Neo Divitias/Assets/Scripts/Menu and Game State/ToggleScript.cs
+++ Neo Divitias/Assets/Scripts/Menu and Game State/ToggleScript.cs
@@ -143,66 +143,66 @@ public class ToggleScript : MonoBehaviour {
 
     }
 
-    public void Refresh(){
-    TextMeshProUGUI mText = gameObject.GetComponentsInChildren<TextMeshProUGUI>()[0];
-    TextMeshProUGUI cash_left = transform.parent.parent.GetComponentsInChildren<TextMeshProUGUI>()[11];
-    Text cost = gameObject.GetComponentsInChildren<Text>()[0];
-    Button upgrade = gameObject.GetComponentsInChildren<Button>()[0];
+    public void Refresh() {
 
-    string item_name = gameObject.name;
-    int current_level = 0;
-    int money = 0;
-    bool upgradeable = false;
-    bool affordable = false;
+        TextMeshProUGUI mText = gameObject.GetComponentsInChildren<TextMeshProUGUI>()[0];
+        TextMeshProUGUI cash_left = transform.parent.parent.GetComponentsInChildren<TextMeshProUGUI>()[11];
+        Text cost = gameObject.GetComponentsInChildren<Text>()[0];
+        Button upgrade = gameObject.GetComponentsInChildren<Button>()[0];
 
-    if (gameObject.layer == 8){
-        current_level = GameState.player_one.Equipment[item_name.ToLower()];
-        money = GameState.player_one.money;
-    }
-    else if (gameObject.layer == 9){
-        current_level = GameState.player_two.Equipment[item_name.ToLower()];
-        money = GameState.player_two.money;
-    }
+        string item_name = gameObject.name;
+        int current_level = 0;
+        int money = 0;
+        bool upgradeable = false;
+        bool affordable = false;
 
-    // Check if weapon is at max level or not
-    int cost_of_upgrade = PlayerPrefs.GetInt(string.Format("{0}_{1}", item_name.ToLower(), current_level + 1));
-    if (cost_of_upgrade != 0){
-        upgradeable = true;
-        if (cost_of_upgrade <= money){
-            affordable = true;
+        if (gameObject.layer == 8){
+            current_level = GameState.player_one.Equipment[item_name.ToLower()];
+            money = GameState.player_one.money;
         }
-    }
+        else if (gameObject.layer == 9){
+            current_level = GameState.player_two.Equipment[item_name.ToLower()];
+            money = GameState.player_two.money;
+        }
+
+        // Check if weapon is at max level or not
+        int cost_of_upgrade = PlayerPrefs.GetInt(string.Format("{0}_{1}", item_name.ToLower(), current_level + 1));
+        if (cost_of_upgrade != 0){
+            upgradeable = true;
+            if (cost_of_upgrade <= money){
+                affordable = true;
+            }
+        }
         
-    // Disable if max level and replace price with MAX
-    if (upgradeable){
-        cost.text = string.Format("{0}", PlayerPrefs.GetInt(string.Format("{0}_{1}", item_name.ToLower(), current_level + 1)));
-    }
-    else
-    {
-        cost.text = string.Format("MAX");
-        upgrade.interactable = false;
-        // Was a bug where if item became too expensive or max, it would become unclickable. However this would also mean you can select more things on controller.
-        // In this case, just move the selection to the actual toggle
-        toggle.Select();
-    }
+        // Disable if max level and replace price with MAX
+        if (upgradeable){
+            cost.text = string.Format("{0}", PlayerPrefs.GetInt(string.Format("{0}_{1}", item_name.ToLower(), current_level + 1)));
+        }
+        else
+        {
+            cost.text = string.Format("MAX");
+            upgrade.interactable = false;
+            // Was a bug where if item became too expensive or max, it would become unclickable. However this would also mean you can select more things on controller.
+            // In this case, just move the selection to the actual toggle
+            toggle.Select();
+        }
         
-    // Can't click if cant afford
-    if (!affordable){
-        upgrade.interactable = false;
-        toggle.Select();
-    }
+        // Can't click if cant afford
+        if (!affordable){
+            upgrade.interactable = false;
+        }
 
 
-    if (current_level == 0)
-    {
-        toggle.interactable = false;
-    }
-    else
-    {
-        toggle.interactable = true;
-    }
-    mText.text = string.Format("{0}", current_level);
-    cash_left.text = string.Format("$ {0}", money);
+        if (current_level == 0)
+        {
+            toggle.interactable = false;
+        }
+        else
+        {
+            toggle.interactable = true;
+        }
+        mText.text = string.Format("{0}", current_level);
+        cash_left.text = string.Format("$ {0}", money);
 
     }
 }
