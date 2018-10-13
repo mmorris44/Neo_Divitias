7bdd93d5ba23d1e93a17ebbc1893d97812e757fa
Michael White <mike.james.white@icloud.com>
2018-10-13 17:19:33 +0200

    Final fade to white implemented.

--- Neo Divitias/Assets/CutsceneFader.cs
+++ Neo Divitias/Assets/CutsceneFader.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CutsceneFader : MonoBehaviour {
     public Animator animator;
     public Image img;
+    public bool fadeOnStart = true;
 
 	void Start () {
-        animator.Play("FadeIn");
+        if (fadeOnStart)
+            animator.Play("FadeIn");
+        else
+        {
+            StartCoroutine(FadeOutSlow());
+        }
 	}
 	
 	public void FadeOut () {
@@ -18,5 +25,21 @@ public class CutsceneFader : MonoBehaviour {
     public bool FadedOut()
     {
         return img.color.a == 1f;
+    }
+
+    public IEnumerator FadeOutSlow()
+    {
+        animator.Play("FadeOutSlow");
+        float wait_until = Time.time + 37f;
+        while (Time.time < wait_until)
+        {
+            yield return null;
+
+        }
+        Color c = img.color;
+        c.a = 1f;
+        img.color = c;
+
+        SceneManager.LoadSceneAsync("Main");
     }
 }
