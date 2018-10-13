81bda0f59750f1461b6d61f52197b4abd658ddfd
Matthew Morris <MRRMAT011@myuct.ac.za>
2018-10-13 14:43:09 +0200

    Final cutscene basics

---
+++ Neo Divitias/Assets/Scripts/Effects/ScrollingText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingText : MonoBehaviour {
+
+    public float scrollX = 0.5f;
+    public float scrollY = 0.5f;
+
+    Material material;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update () {
+        float offsetX = Time.time * scrollX;
+        float offsetY = Time.time * scrollY;
+
+        material.mainTextureOffset = new Vector2(offsetX, offsetY);
+	}
+}
