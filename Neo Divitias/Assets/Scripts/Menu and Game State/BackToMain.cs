aab7cb801de36ffcd5b1129fbc835c42d504ff6c
Adrian Krige <krigeadrian@gmail.com>
2018-10-14 15:48:51 +0200

    Fix controls to main

---
+++ Neo Divitias/Assets/Scripts/Menu and Game State/BackToMain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMain : MonoBehaviour {
+    public GameObject main;
+    public GameObject controls;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Back"))
+        {
+            main.SetActive(true);
+            controls.SetActive(false);
+        }
+    }
+}
