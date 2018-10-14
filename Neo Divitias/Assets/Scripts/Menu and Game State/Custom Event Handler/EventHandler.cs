47afc8a38069d5347842d8cf4f3ef5b69517322c
Matthew Morris <MRRMAT011@myuct.ac.za>
2018-10-14 12:07:55 +0200

    Players can no longer interfere with each other in menu. FINALLY FFS UNITY API GET YOUR ACT TOGETHER YOU SORRY EXCUSE FOR A GAME ENGINE

--- Neo Divitias/Assets/Scripts/Menu and Game State/Custom Event Handler/EventHandler.cs
+++ Neo Divitias/Assets/Scripts/Menu and Game State/Custom Event Handler/EventHandler.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class EventHandler : EventSystem {
+
+    public int playerLayer;
+
+    GameObject lastSelectedGameObject;
+
+    protected override void Start()
+    {
+        base.Start();
+        lastSelectedGameObject = currentSelectedGameObject;
+    }
+
     protected override void OnEnable()
     {
         // do not assign EventSystem.current
@@ -16,5 +28,14 @@ public class EventHandler : EventSystem {
         current = this; // in order to avoid reimplementing half of the EventSystem class, just temporarily assign this EventSystem to be the globally current one
         base.Update();
         current = originalCurrent;
+
+        // Check for if legal selection
+        if (currentSelectedGameObject.layer != playerLayer)
+        {
+            SetSelectedGameObject(lastSelectedGameObject);
+        } else
+        {
+            lastSelectedGameObject = currentSelectedGameObject;
+        }
     }
 }
